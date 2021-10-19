d859a96cc689c1643aeede65ac74dc28820cfed4
Manan Dudhwala <manandudhwala9201@gmail.com>
2021-10-19 11:05:47 +0530

    Update reader-writer-threading.py

--- code/reader-writer-threading.py
+++ code/reader-writer-threading.py
@@ -1,4 +1,3 @@
-??? from here until ???END lines may have been inserted/deleted
 import threading
 import random
 
@@ -8,34 +7,41 @@ wlock = threading.Lock()
 rlock = threading.Lock()
 
 def Reader():
-        global x
-            try:
-                        wlock.acquire()
-                                print('Reader is Reading!')
-                                        print('Shared Data:', x)
-                                                wlock.release()
-                                                        print()
-                                                            except:
-                                                                        print("Reader couldn't read, as Writing is taking place.")
+    global x
+    try:
+        wlock.acquire()
+        print('Reader is Reading!')
+        print('Shared Data:', x)
+        wlock.release()
+        print()
+    except:
+        print("Reader couldn't read, as Writing is taking place.")
 
-                                                                        def Writer():
-                                                                                global x
-                                                                                    try:
-                                                                                                wlock.acquire()
-                                                                                                        rlock.acquire()
-                                                                                                                print('Writer is Writing!')
-                                                                                                                        x += 1
-                                                                                                                                print('Writer is Releasing the lock!')
-                                                                                                                                        wlock.release()
-                                                                                                                                                rlock.release()
-                                                                                                                                                        print()
-                                                                                                                                                            except:
-                                                                                                                                                                        print("Writer couldn't write, as Reading or Writing is taking place.")
+def Writer():
+    global x
+    try:
+        wlock.acquire()
+        rlock.acquire()
+        print('Writer is Writing!')
+        x += 1
+        print('Writer is Releasing the lock!')
+        wlock.release()
+        rlock.release()
+        print()
+    except:
+        print("Writer couldn't write, as Reading or Writing is taking place.")
 
-                                                                                                                                                                        if __name__ == '__main__':
-                                                                                                                                                                                for i in range(0, 10):
-                                                                                                                                                                                            randomNumber = random.randint(0, 100) 
-                                                                                                                                                                                                    if(randomNumber > 50):
-                                                                                                                                                                                                                    threading1 = threading.Thread(target = Reader)
-                                                                                                                                                                                                                                threading1.start()
-???END
+if __name__ == '__main__':
+    for i in range(0, 10):
+        randomNumber = random.randint(0, 100) 
+        if(randomNumber > 50):
+            threading1 = threading.Thread(target = Reader)
+            threading1.start()
+        else:
+            threading2 = threading.Thread(target = Writer)
+            threading2.start()
+
+threading1.join()
+threading2.join()
+
+print("Final value : ",x)
